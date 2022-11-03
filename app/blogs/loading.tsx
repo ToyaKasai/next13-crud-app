cb027e13793d74c75eacfc44d3aad7435be35efe
ToyaKasai <kasaitoya@team-lab.com>
2022-11-04 06:17:55 +0900

    feat: add loading

---
+++ app/blogs/loading.tsx
@@ -0,0 +1,12 @@
+import Container from "../../components/common/container/Container";
+import { FC } from "react";
+
+const BlogsLoading: FC = () => {
+  return (
+    <Container className="flex min-h-screen items-center justify-center">
+      Loading...
+    </Container>
+  );
+};
+
+export default BlogsLoading;
